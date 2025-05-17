201352a5411f1164e73fe20713b5a7d3f1e1434c
Ilia Miheev <imiheev774@gmail.com>
2025-05-17 15:46:32 +0300

    Версия обновлена до 3.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ipars',
-    version='3.1.3',
+    version='3.1.4',
     description='Библиотека для работы с файлами во время парсинга',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
