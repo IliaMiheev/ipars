1718d7bcecf98ed3d57152e64d9780853ac87cb5
Ilia Miheev <imiheev774@gmail.com>
2025-10-01 20:17:04 +0300

    Добавлен класс для замера времени выполнения скрипта

--- ipars/__init__.py
+++ ipars/__init__.py
@@ -2,3 +2,4 @@ from .ParsManagerCode import Pars
 from .JsonManagerCode import JsonManager
 from .CsvManagerCode import CsvManager
 from .ProgressBarCode import ProgressBarManager
+from .TimerManagerCode import TimerManager
