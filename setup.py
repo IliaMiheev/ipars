47450451eefaa89135358ed6cdeb9ebfda31b9b7
Ilia Miheev <imiheev774@gmail.com>
2025-11-14 18:59:48 +0300

    Версия приложения обновлена до 3.9.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ipars',
-    version='3.9.0',
+    version='3.9.1',
     description='Библиотека для работы с файлами во время парсинга',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
